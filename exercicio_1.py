47c0db049ea8d04003de427736b8820771590dd5
Eric <ericsm123@gmail.com>
2025-04-04 13:32:50 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,15 +1,11 @@
-"""
-#### Exercício
+numero = int(input("Digite um número: "))
 
-Dado um número digitado pelo usuário, imprima seu antecessor, seu sucessor, seu dobro e sua raiz quadrada
+antecessor = numero - 1 
+sucessor = numero + 1
+dobro = numero * 2
+raiz = numero **(1/2)
 
-Exemplo:
-Digite seu número: 4
-
-Resposta:
-
-O antecessor do número 4 é 3.
-O sucessor do número 4 é 5.
-O dobro do número 4 é 8.
-A raiz quadrada do número 4 é 2.
-"""
+print(f"O antecessor do número {numero} é {antecessor}")
+print(f"O sucessor do número {numero} é {sucessor}")
+print(f"O dobro do número {numero} é {dobro}")
+print(f"O raiz quadrada do número {numero} é {raiz}")
